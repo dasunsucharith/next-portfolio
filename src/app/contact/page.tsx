e8913d8a7380c69201c59a2117811e0685c53957
Dasun Sucharith <sucharith.dasun@gmail.com>
2025-06-05 12:13:13 +0530

    Add contact form with EmailJS

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import NavBar from "@/components/NavBar";
+import ContactForm from "@/components/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 dark:bg-black">
+      <NavBar />
+      <section className="flex min-h-screen items-center justify-center p-4" id="contact">
+        <ContactForm />
+      </section>
+    </main>
+  );
+}
